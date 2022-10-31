272e4686f0fb27ba8297ac9e4e4a62ebbafe43da
Rocky <rocky.margin0124@gmail.com>
2022-10-31 14:19:11 -0400

    init

---
+++ includes/handlers/ajax/config.php
@@ -0,0 +1,9 @@
+<?php
+	ob_start();
+	session_start();
+	$timezone = date_default_timezone_set("Europe/London");
+	$con = mysqli_connect("localhost", "root", "", "geet");
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . mysqli_connect_errno();
+	}
+?>
---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<?php
+	include("includes/config.php");
+	include("includes/classes/User.php");
+	include("includes/classes/Artist.php");
+	include("includes/classes/Album.php");
+	include("includes/classes/Song.php");
+	include("includes/classes/Playlist.php");
+
+//session_destroy(); LOGOUT
+
+if(isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = new User($con, $_SESSION['userLoggedIn']);
+	$username = $userLoggedIn->getUsername();
+	echo "<script>userLoggedIn = '$username';</script>";
+}
+else {
+	header("Location: register.php");
+}
+
+?>
+
+<html>
+<head>
+	<title>Welcome to Geet! Your one stop destination for music</title>
+	<link rel="icon" href="../assets/images/icons/play.png" type="image/x-icon"/>
+	<link rel="shortcut icon" href="../assets/images/icons/play.png" type="image/x-icon"/>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/script.js"></script>
+</head>
+
+<body>
+
+	<div id="mainContainer">
+
+		<div id="topContainer">
+
+			<?php include("includes/navBarContainer.php"); ?>
+
+			<div id="mainViewContainer">
+
+				<div id="mainContent">
